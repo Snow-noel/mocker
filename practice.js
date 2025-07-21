5076fe6e9a2ec6b7d34dda9e1022bfe69391ef0c
Snow <bis23-smsumba@mubas.ac.mw>
2025-07-21 14:06:14 +0200

    initial commit

---
+++ practice.js
@@ -0,0 +1,20 @@
+const massage = document.getElementById("h1");
+const main= document.getElementById("main");
+const kid = document.getElementById('kid');
+const parent = document.getElementById("pare");
+const span=document.getElementById('span');
+
+setTimeout(() => {
+    parent.style.display = "none";
+    massage.textContent="Am sorry guys am just a single guy";
+    span.style.display="none";
+    main.style.backgroundImage="url(../pic/mine.jpg)"
+    main.style.backgroundRepeat = "no-repeat";
+    main.style.backgroundPosition = "center";
+    main.style.backgroundSize = "cover";
+    main.style.borderRadius="10px"
+    
+}, 60000);
+
+
+
